c66924a35fe2ebe14e44fdd2ff88c4ee933762b8
Michael Morris <mmorris1990@gmail.com>
2019-07-02 18:34:11 -0500

    required spotify and axios modules. got bandsInTown API to work and display response.data

--- liri.js
+++ liri.js
@@ -4,11 +4,18 @@ require("dotenv").config();
 // import the keys.js file and store it in a variable
 var keys = require("./keys.js");
 
+// access the spotify api module
+var Spotify = require('node-spotify-api');
+
+// access the axios module
+var axios = require("axios");
+
 // access keys information
-// var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 // take in the CLI input 
 var input = process.argv[2];
+var search = process.argv[3];
 
 // empty variable to output from the switch
 var command;
@@ -17,18 +24,22 @@ var command;
 switch (input) {
     case "concert-this":
         command = input;
+        bandsInTown();
         break;
 
     case "spotify-this-song":
         command = input;
+        songSearch();
         break;
 
     case "movie-this":
         command = input;
+        movieSearch();
         break;
 
     case "do-what-it-says":
         command = input;
+        randomSearch();
         break;
 
     default:
@@ -37,3 +48,24 @@ switch (input) {
 
 // prints the command
 console.log(command + " success!");
+
+// FUNCTIONS
+
+function bandsInTown() {
+    axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(
+        function(response) {
+            console.log(response.data);
+        }
+    )};
+
+function songSearch() {
+
+};
+
+function moveiSearch() {
+
+};
+
+function randomSearch() {
+
+};
